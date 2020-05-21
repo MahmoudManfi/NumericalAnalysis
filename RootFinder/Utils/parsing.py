160e127ad394402471d5ec4887ebc38e7900c235
Ahmed Alzayady <ahmedalzayady295@gmail.com>
2020-05-21 04:01:29 +0200

    get coff

--- RootFinder/Utils/parsing.py
+++ RootFinder/Utils/parsing.py
@@ -1,3 +1,17 @@
+from sympy import simplify, expand
+
+
+def convert_num(term):
+    is_float = False
+    for i in term:
+        if i == '.':
+            is_float = True
+            break
+    if is_float:
+        return float(term)
+    return int(term)
+
+
 def is_num(num):
     if (isinstance(num, complex)):
         return True
@@ -6,9 +20,9 @@ def is_num(num):
     if (isinstance(num, int)):
         return True
     for i in num:
-        if i=='.':
+        if i == '.':
             continue
-        if i < '0' or i > '9' :
+        if i < '0' or i > '9':
             return False
     return True
 
@@ -24,7 +38,7 @@ def get_prant(str, start):
         elif str[start] == ')':
             open -= 1
         start += 1
-        if start==len(str) and open!=0:
+        if start == len(str) and open != 0:
             raise Exception('bad input ')
     return term
 
@@ -56,19 +70,19 @@ def add_arth(terms, op):
             terms.append(op)
         return
     if terms[size - 1] in high_operator:
-        if op=='+':
+        if op == '+':
             return
-        elif op=='-':
+        elif op == '-':
             terms.append('-')
             return
         else:
-          raise Exception('bad input ')
+            raise Exception('bad input ')
     if terms[size - 1] == '-':
         terms.pop()
         if op == '+':
             terms.append('-')
         else:
-            if size==1 or not is_num(terms[size-2]) :
+            if size == 1 or not is_num(terms[size - 2]):
                 return
             terms.append('+')
         return
@@ -115,9 +129,67 @@ def parse(str):
                 i += 1
             i -= 1
             terms.append(term)
-        elif str[i]!=' ':
+        elif str[i] != ' ':
             raise Exception('bad input ')
-        i+=1
+        i += 1
     return terms
 
 
+def get_coefficient(function_string):
+    function_string = str(expand(function_string))
+    print(function_string)
+    terms = parse(function_string)
+    print(terms)
+    index = 0
+    size = len(terms)
+    ans = []
+    large = -1
+    next_power = -1
+    while index < size:
+        if terms[index] == 'x':
+            if index == 0 or terms[index - 1] == '+':
+                coff = 1
+            else:
+                num = convert_num(terms[index - 2])
+                if index == 1 or terms[index - 3] == '+':
+                    coff = num
+                else:
+                    coff = -1 * num
+
+            if large == -1:
+                if terms[index + 1] == '**':
+                    large = convert_num(terms[index + 2])
+                else:
+                    large = 1
+                next_power = large - 1
+
+            else:
+                if terms[index + 1] != '**':
+                    next = 1
+                else:
+                    next = convert_num(terms[index + 2])
+                temp_next=next
+                while next != next_power:
+                    ans.append(0)
+                    next += 1
+                next_power = temp_next - 1
+
+            ans.append(coff)
+
+        index += 1
+
+    while next_power!=0:
+        ans.append(0)
+        next_power-=1
+    if size<2:
+        ans.append(convert_num(terms[size-1]))
+    else:
+        if terms[size-2]=='**':
+            ans.append(0)
+        else:
+            print(terms[size-1])
+            ans.append(convert_num(terms[size-1]))
+    return ans
+
+kk = get_coefficient("x**5-2*x**2")
+print(kk)
